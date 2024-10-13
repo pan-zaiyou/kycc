eca2e12982acff53b6a955d9be71d029e8b543db
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-10-13 16:44:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.config = {
   logo: '/static/img/logo6.svg', // 网站logo
   logo2: '', // 网站logo
   language: 'zh', //设置默认访问语言：zh, en
-  crisp: '1d4639a7-3e57-4706-a7f7-5968179bcdde', //填写了会加载crisp
+  crisp: '0d31a6be-2276-432f-bd47-ac8d962e84ae', //填写了会加载crisp
   currency: 'CNY', //货币符号
   currency_symbol: '￥', //货币单位
   telegram_discuss_link: 'https://t.me/Heycloud_official666', //TG频道
